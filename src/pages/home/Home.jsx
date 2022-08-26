a2e0456b356598cc4dd1d18857c379607aa1c10f
Uzifat <89532700+Uzifat@users.noreply.github.com>
2022-08-26 23:10:41 +0700

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,30 +11,30 @@ import Blog from "../../components/blog/Blog";
 const fakeDataBlog = [
     {
         id: 1,
-        name: 'Bai hoc so 1 o day',
-        des: 'Lorem ipsum, dolor sit amet consectetur adipisicingelit. Architecto aperiam aut dolorum cumque eavoluptatibus dolorem deserunt commodi impeditConsequatur!elit. Architecto aperiam aut dolorum cumque eavoluptatibus dolorem deserunt commodi impedit? Consequatur!',
+        name: 'Có bầu 4 tháng có quan hệ được không',
+        des: 'Có bầu 4 tháng quan hệ được không? Chắc hẳn đây là mối băn khoăn của hầu hết các cặp vợ chồng. Liệu khi quan hệ có ảnh hưởng gì đến em bé trong bụng không?',
         star: 5,
-        img: 'https://kynguyenlamdep.com/wp-content/uploads/2022/06/anh-gai-xinh-cuc-dep.jpg',
-        author: 'Khanh',
-        time: '26/8/2022'
+        img: 'https://www.fvhospital.com/wp-content/uploads/2018/03/dr-vo-trieu-dat-2020.jpg',
+        author: 'Quan',
+        time: '20/8/2022'
     },
     {
         id: 2,
-        name: 'Bai hoc so 2 o day',
-        des: 'Lorem ipsum, dolor sit amet consectetur adipisicingelit. Architecto aperiam aut dolorum cumque eavoluptatibus dolorem deserunt commodi impeditConsequatur!elit. Architecto aperiam aut dolorum cumque eavoluptatibus dolorem deserunt commodi impedit? Consequatur!',
+        name: 'Đau rát tầng sinh môn khi quan hệ là bệnh gì ?',
+        des: 'Có rất nhiều nguyên nhân gây đau rát tầng sinh môn khi quan hệ, đây là vùng nhạy cảm dễ bị tổn thương của phụ nữ. Tình trạng này có thể bị gây ra bởi tình trạng nhiễm trùng vùng kín, hoặc có thể là do một số loại bệnh lây truyền qua đường tình dục.',
         star: 4 ,
-        img: 'https://kynguyenlamdep.com/wp-content/uploads/2022/06/anh-gai-xinh-cuc-dep.jpg',
-        author: 'Khanh',
-        time: '26/8/2022'
+        img: 'https://www.fvhospital.com/wp-content/uploads/2018/03/dr-vo-trieu-dat-2020.jpg',
+        author: 'Tam',
+        time: '10/8/2022'
     },
     {
         id: 3,
-        name: 'Bai hoc so 3 o day',
-        des: 'Lorem ipsum, dolor sit amet consectetur adipisicingelit. Architecto aperiam aut dolorum cumque eavoluptatibus dolorem deserunt commodi impeditConsequatur!elit. Architecto aperiam aut dolorum cumque eavoluptatibus dolorem deserunt commodi impedit? Consequatur!',
+        name: 'Uống thuốc tránh thai hàng ngày có thai không?',
+        des: 'Uống thuốc tránh thai hàng ngày là một trong những lựa chọn được các cặp đôi dùng khi muốn ngừa thai hiệu quả, Tuy nhiên, bên cạnh lợi ích của thuốc mang lại thì vẫn còn không ít những băn khoăn, thắc mắc của chị em khi dùng thuốc, trong đó câu hỏi được thắc mắc nhiều nhất là ” Uống thuốc tránh thai hàng ngày có thai không?',
         star: 2,
-        img: 'https://kynguyenlamdep.com/wp-content/uploads/2022/06/anh-gai-xinh-cuc-dep.jpg',
-        author: 'Khanh',
-        time: '26/8/2022'
+        img: 'https://www.fvhospital.com/wp-content/uploads/2018/03/dr-vo-trieu-dat-2020.jpg',
+        author: 'Phuong',
+        time: '01/07/2019'
     },
 ]
 
